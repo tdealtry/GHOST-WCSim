dfd05873117267eb6c1a928af29276d2c9b609e3
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2024-04-26 11:20:25 +0100

    Move the WCSimTrackingAction code into HKG4TrackingAction & multiply inherit from Tool & G4UserTrackingAction. Untested (like last couple of commits) since I forgot to add new tools into the toolchain until now, and now I need to toolify more tools to setup G4 user classes in the correct order

--- HKG4TrackingAction/HKG4TrackingAction.h
+++ HKG4TrackingAction/HKG4TrackingAction.h
@@ -8,6 +8,7 @@
 #include "Tool.h"
 
 #include "G4UImanager.hh"
+#include "G4UserTrackingAction.hh"
 
 #include "WCSimTrackingAction.hh"
 
@@ -22,21 +23,49 @@
 namespace HK {
 	namespace Ghost {
 		namespace G4 {
-			class HKG4TrackingAction : public Tool {
+			class HKG4TrackingAction : public Tool, public G4UserTrackingAction {
 
 			public:
 
 				HKG4TrackingAction();  ///< Simple constructor
 				bool Initialise(std::string configfile,
-												DataModel& data);  ///< Initialise Function for setting up Tool resources. @param
+												DataModel& data) override;  ///< Initialise Function for setting up Tool resources. @param
 				///< configfile The path and name of the dynamic configuration file
 				///< to read in. @param data A reference to the transient data
 				///< class used to pass information between Tools.
-				bool Execute();                    ///< Execute function used to perform Tool purpose.
-				bool Finalise();                   ///< Finalise funciton used to clean up resources.
+				bool Execute() override;                    ///< Execute function used to perform Tool purpose.
+				bool Finalise() override;                   ///< Finalise funciton used to clean up resources.
 
 			private:
 				G4UImanager* m_p_UI;
+
+			public:
+				void PreUserTrackingAction (const G4Track* aTrack) override;
+				void PostUserTrackingAction(const G4Track*) override;
+
+				void SetFractionChPhotons(G4double fraction){percentageOfCherenkovPhotonsToDraw = fraction;}
+  
+				void AddProcess(const G4String &process){ProcessList.insert(process);}
+				void AddParticle(G4int pid){ParticleList.insert(pid);}
+				void SetSaveHitProducingTracks(G4bool save){saveHitProducingTracks = save;}
+				G4bool GetSaveHitProducingTracks() const {return saveHitProducingTracks;}
+
+			private:
+				std::set<G4String> ProcessList;
+				std::set<G4int> ParticleList;
+				G4bool saveHitProducingTracks = true;
+  
+				G4double fTime_birth;
+				G4double fMaxTime;
+				// TF: define in macro now
+				G4double percentageOfCherenkovPhotonsToDraw;
+
+				bool SAVE_PHOTON_HISTORY;
+
+				WCSimTrackingMessenger* messenger;
+  
+				G4int primaryID;
+				
 			};
 		} // namespace G4
 	} // namespace Ghost
