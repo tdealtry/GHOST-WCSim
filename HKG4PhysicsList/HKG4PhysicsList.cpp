bf3083d0066d2d41002be2b733acd30b06160335
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2024-04-30 09:39:33 +0100

    Don't understand why the RunAction pointer disappears in Finalise...

--- HKG4PhysicsList/HKG4PhysicsList.cpp
+++ HKG4PhysicsList/HKG4PhysicsList.cpp
@@ -35,18 +35,31 @@ bool HKG4PhysicsList::Initialise(std::string configfile, DataModel& data) {
 	// Initialize the physics factory to register the selected physics.
 	m_p_phys_factory->InitializeList();
 	m_data->m_p_run_manager->SetUserInitialization(m_p_phys_factory);
-																				 
+
+	auto p_run_action = static_cast<const WCSimRunAction*>(m_data->m_p_run_manager->GetUserRunAction());
+	*m_log << ML(0) << "WCSimRunAction pointer is "
+				 <<	p_run_action
+				 << " in Initialise()" << std::endl;
+
 	return true;
 }
 
 bool HKG4PhysicsList::Execute() {
 
+	auto p_run_action = static_cast<const WCSimRunAction*>(m_data->m_p_run_manager->GetUserRunAction());
+	*m_log << ML(0) << "WCSimRunAction pointer is "
+				 <<	p_run_action
+				 << " in Exectute()" << std::endl;
 	return true;
 }
 
 bool HKG4PhysicsList::Finalise() {
 
-	m_p_phys_factory->SaveOptionsToOutput(static_cast<const WCSimRunAction*>(m_data->m_p_run_manager->GetUserRunAction())->GetRootOptions());
+	auto p_run_action = static_cast<const WCSimRunAction*>(m_data->m_p_run_manager->GetUserRunAction());
+	if(p_run_action == nullptr)
+		*m_log << ML(0) << "WCSimRunAction pointer is null in destructor" << std::endl;
+	else
+		m_p_phys_factory->SaveOptionsToOutput(p_run_action->GetRootOptions());
 	
 	return true;
 }
